663f4df7f93397ff9c2d3b7aa067b3fc6e54d3d4
vpuzin <vlad.puzin@gmail.com>
2021-05-03 23:18:26 +0300

    huinya

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
 document.querySelector('.mat-disc-but').addEventListener('click', function (event) {
     event.preventDefault();
 
-    let fv = parseFloat(document.querySelector('.simple-fv').value);
-    let pv = parseFloat(document.querySelector('.simple-pv').value);
-    let n = parseFloat(document.querySelector('.simple-n').value);
-    let r = parseFloat(document.querySelector('.simple-r').value);
+    let fv = getVariable('.simple-fv');
+    let pv = getVariable('.simple-pv');
+    let n = getVariable('.simple-n');
+    let r = getVariable('.simple-r');
 
     n = n / 360;
     r = r / 100;
 
-    let nullableVariable = findNullable(fv, pv, n, r);
+    let nullableVariable = findNan(fv, pv, n, r);
 
     let result = null;
     switch (nullableVariable) {
@@ -37,15 +37,15 @@ document.querySelector('.mat-disc-but').addEventListener('click', function (even
 document.querySelector('.mat-disc-kom').addEventListener('click', function (event) {
     event.preventDefault();
 
-    let fv = parseFloat(document.querySelector('.simple-fv-kom').value);
-    let pv = parseFloat(document.querySelector('.simple-pv-kom').value);
-    let n = parseFloat(document.querySelector('.simple-n-kom').value);
-    let d = parseFloat(document.querySelector('.simple-d-kom').value);
+    let fv = getVariable('.simple-fv-kom');
+    let pv = getVariable('.simple-pv-kom');
+    let n = getVariable('.simple-n-kom');
+    let d = getVariable('.simple-d-kom');
 
     n = n / 360;
     d = d / 100;
 
-    let nullableVariable = findNullable(fv, pv, n, d);
+    let nullableVariable = findNan(fv, pv, n, d);
 
     let result = null;
     switch (nullableVariable) {
@@ -70,11 +70,83 @@ document.querySelector('.mat-disc-kom').addEventListener('click', function (even
     document.querySelector('.result-kom-disc').innerHTML = result
 });
 
+document.querySelector('.ss-simple-but').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    let fv = getVariable('.ss-simple-fv');
+    let pv = getVariable('.ss-simple-pv');
+    let n = getVariable('.ss-simple-n');
+    let r = getVariable('.ss-simple-r');
+
+    n = n / 360;
+    r = r / 100;
+
+    let nullableVariable = findNan(fv, pv, n, r);
+
+    let result = null;
+    switch (nullableVariable) {
+        case 'fv': {
+            result = 'FV = ' + round(pv * Math.pow((1 + r), n))
+            break;
+        }
+        case 'pv': {
+            result = 'PV = ' + round(fv / Math.pow((1 + r), n))
+            break;
+        }
+        case 'r': {
+            result = 'r = ' + round((Math.pow(fv / pv, 1 / n) - 1) * 100)
+            break;
+        }
+        case 'n': {
+            result = 'n = ' + round((Math.log(fv / pv)) / (1 + r))
+            break;
+        }
+    }
+
+    document.querySelector('.result-ss-simple').innerHTML = result
+});
+
+document.querySelector('.ss-n-but').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    let fv = getVariable('.ss-n-fv');
+    let pv = getVariable('.ss-n-pv');
+    let j = getVariable('.ss-n-j');
+    let n = getVariable('.ss-n-n');
+
+    n = n / 360;
+    j = j / 100;
+
+    let nullableVariable = findNan(fv, pv, n, j);
+
+    let result = null;
+    switch (nullableVariable) {
+        case 'fv': {
+            result = 'FV = ' + round(pv * Math.exp(j * n))
+            break;
+        }
+        case 'pv': {
+            result = 'PV = ' + round(fv / Math.exp(j * n))
+            break;
+        }
+        case 'r': {
+            result = 'j = ' + round((Math.log(fv / pv)) / n)
+            break;
+        }
+        case 'n': {
+            result = 'n = ' + round((Math.log(fv / pv)) / j)
+            break;
+        }
+    }
+
+    document.querySelector('.result-ss-n').innerHTML = result
+});
+
 function round(x) {
     return Math.round(x * 100) / 100;
 }
 
-function findNullable(fv, pv, n, r) {
+function findNan(fv, pv, n, r) {
     if (isNaN(fv)) {
         return 'fv';
     }
@@ -90,4 +162,8 @@ function findNullable(fv, pv, n, r) {
     if (isNaN(r)) {
         return 'r';
     }
+}
+
+function getVariable(boxName) {
+    return parseFloat(document.querySelector(boxName).value);
 }
